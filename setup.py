209ff56484fb369008a95191293831fc3e01a4a6
Alejandro Alcalde <algui91@gmail.com>
2013-03-23 01:23:45 +0100

    Adding setup.py for easing the install
    Now install the module is an easy as run:
    
            python3.2 setup.py install

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+SRC = 'src/'
+
+module1 = Extension('gnm',
+                    include_dirs = ['./include'],
+#                    extra_link_args = ['-shared'],#, '-I/usr/include/python3.2'],
+                    sources = [SRC + 'gnmModule.c'])
+
+setup (name = 'GraphicNetworkMonitoring',
+       version = '0.1',
+       description = 'GNM - Graphic Network Monitoring tool',
+       author = 'Alejandro Alcalde',
+       author_email = 'algui91@gmail.com',
+       url = 'https://github.com/algui91/GraphicNetworkMonitoring',
+       license = 'GPLv3',
+       long_description = '''
+       TODO
+       ''',
+       ext_modules = [module1])
